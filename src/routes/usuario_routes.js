5d84b3082889400ef9ee6d99e704308adc06a55e
johan <jflorez@truora.com>
2020-04-26 17:09:05 -0500

    Archivos para el flow de serivicios
    JOHAN

--- src/routes/usuario_routes.js
+++ src/routes/usuario_routes.js
@@ -3,7 +3,7 @@ let usuario = require('../controllers/usuario');
 let Usuario_controller = usuario.Usuario_controller;
 
 // metodo para poder crear un usuario en la base de datos
-exports.crear_usuario = async(req, res) => {
+exports.crear_usuario = async (req, res) => {
     //obtenemos los campos de la solicitud
     id = req.body.id;
     nombre = req.body.nombre;
@@ -14,6 +14,7 @@ exports.crear_usuario = async(req, res) => {
     longitud = req.body.longitud;
     direccion = req.body.direccion;
     contrasenha = req.body.contrasenha;
+
     usuario_controller = new Usuario_controller();
     let data = usuario_controller.crear_usuario(id, nombre, apellido, celular, correo, latitud, longitud, direccion, id, id, contrasenha, true);
 
@@ -22,24 +23,25 @@ exports.crear_usuario = async(req, res) => {
             message: 'El id, el celular, la latitud y longitud deben ser datos númericos',
             status: 400
         });
-    } else {
-        //resolvemos la promesa
-        data.then(result => {
-            res.json(result);
-        }).catch(err => {
-            res.json({
-                message: err,
-                status: 500
-            });
-        });
     }
+
+    //resolvemos la promesa
+    data.then(result => {
+        res.json(result);
+    }).catch(err => {
+        res.json({
+            message: err,
+            status: 500
+        });
+    });
 };
 
 // metodo para poder loguearnos como usuarios a la base de datos
-exports.login_usuario = async(req, res) => {
+exports.login_usuario = async (req, res) => {
     //obtenemos los campos de la solicitud
     id = req.body.id;
     contrasenha = req.body.contrasenha;
+
     usuario_controller = new Usuario_controller();
     let data = usuario_controller.usuario_login(id, contrasenha);
 
@@ -48,15 +50,15 @@ exports.login_usuario = async(req, res) => {
             message: 'El id debe ser tipo numérico y la contraseña no puede ser vacia',
             status: 400
         });
-    } else {
-        //resolvemos la promesa
-        data.then(result => {
-            res.json(result);
-        }).catch(err => {
-            res.json({
-                message: err,
-                status: 500
-            });
-        });
     }
+
+    //resolvemos la promesa
+    data.then(result => {
+        res.json(result);
+    }).catch(err => {
+        res.json({
+            message: err,
+            status: 500
+        });
+    });
 }
